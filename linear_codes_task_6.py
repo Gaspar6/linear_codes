a4529ee80bbe7fc3bfda62d05adfe4e09b62ce72
unknown <you@example.comgaspar.sekula.stud@pw.edu.pl>
2023-04-28 21:21:34 +0200

    Linear Codes Task 6

---
+++ linear_codes_task_6.py
@@ -0,0 +1,35 @@
+def linear_combination(base, k):
+    # all linear combinations of vectors from base in field Zk
+
+    Z_k = [ i for i in range(k)]
+    result = []
+
+    for a in Z_k:
+        for b in Z_k:
+            for c in Z_k:
+
+                vector = []
+
+                for i in range(len(base[0])):
+                    vector.append( a * base[0][i] + b * base[1][i] + c * base[2][i])
+
+                for i in range(len(vector)):
+                    vector[i] = vector[i] % k
+                if vector not in result:
+                    result.append(vector)
+    return result
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Generowanie")
+
+    B = [[1, 0, 0, 2, 4], [0, 1, 0, 1, 0], [0, 0, 1, 5, 6]]
+    k = 7
+
+
+    print(linear_combination(B, k))
+
