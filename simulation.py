1233d23df5e3520507fb9a8d591f672ff14df6d6
unknown <you@example.comgaspar.sekula.stud@pw.edu.pl>
2023-05-10 21:47:05 +0200

    (8) decoding

--- simulation.py
+++ simulation.py
@@ -3,17 +3,28 @@ import random
 
 
 def z_n_field(X, n):
+    """
+    :param X: matrix
+    :param n: determines Z_n field
+    :return: modified matrix, so that its elements belong to Z_n field
+    """
     for i in range(len(X)):
         for j in range(len(X[0])):
             X[i][j] %= n
     return X
 
-def generate(nrow, ncol):
-    A = [[random.randint(0, 4) for i in range(ncol)] for j in range(nrow)]
+def generate(nrow, ncol, a, b):
+    """
+    :return: matrix nrow by ncol, which elements are integers i: a <= i <= b
+    """
+    A = [[random.randint(a, b) for i in range(ncol)] for j in range(nrow)]
     return A
 
 
 def norm(M, k):
+    """
+    :return: normalized matrix M in Z_(k+1) field
+    """
     X = copy.deepcopy(M)
     for i in range(len(X)):
         for j in range(len(X[0])):
@@ -22,6 +33,12 @@ def norm(M, k):
 
 
 def return_v(A, k):
+    """
+    :param A: matrix
+    :param k: number of desired matrix A column (counting from 0)
+    :return: (k+1)-th column from matrix A
+    """
+    assert 0 <= k <= len(A[0]) - 1
     result = []
     for i in range(len(A)):
         result.append(A[i][k])
@@ -29,6 +46,12 @@ def return_v(A, k):
 
 
 def code_v(v, G):
+    """
+    :param v: vector to be coded
+    :param G: coding matrix
+    :return: coded vector v
+    """
+    assert len(v) == len(G)
     result = [0 for i in range(len(G[0]))]
     for i in range(len(result)):
         value = 0
@@ -39,6 +62,11 @@ def code_v(v, G):
 
 
 def code_matrix(A, G):
+    """
+    :param A: matrix to be coded
+    :param G: coding matrix
+    :return: coded matrix A
+    """
     result = [[0 for i in range(len(A[0]))] for j in range(len(G[0]))]
     for k in range(len(result[0])):
         v = return_v(A, k)
@@ -48,25 +76,28 @@ def code_matrix(A, G):
             result[i][k] = w[i]
     return result
 
-def send(M):
+def send(M, k):
+    """
+    :param M: matrix to be sent
+    :param k: determines Z_k field
+    :return: simulates a matrix transmission with disturbances (5/100 chance that 3 will be modulo k added)
+    """
     result = copy.deepcopy(M)
     for i in range(len(result)):
         for j in range(len(result[0])):
             if random.uniform(0,1) < 0.95:
-                result[i][j] = result[i][j] % 5
+                result[i][j] = result[i][j] % k
             else:
-                result[i][j] = (result[i][j] + 3) % 5
+                result[i][j] = (result[i][j] + 3) % k
     return result
 
 
 if __name__ == '__main__':
-    print("Symulacja")
-
     random.seed(2137)
 
     # a
-    A = generate(4, 10)
-    print("wygenerowana macierz A:")
+    A = generate(4, 10, 0, 4)
+    print("generating A:")
     for i in A:
         print(i)
 
@@ -76,12 +107,12 @@ if __name__ == '__main__':
 
     # b
     X = norm(A, 4)
-    print("X = unormowana macierz A:")
+    print("X = normalized A:")
     for i in X:
         print(i)
 
     # d
-    print("wektor v:")
+    print("vector v:")
     v = return_v(A, 0)
     for i in range(len(v)):
         v[i] %= 5
@@ -93,7 +124,7 @@ if __name__ == '__main__':
                 [0, 0, 0, 1, 1, 0, 0, 0, 4, 3, 0]]
 
     # d
-    print("zakodowany wektor v")
+    print("coded wektor v")
     coded_v = code_v(v, matrix_G)
     for i in range(len(coded_v)):
         coded_v[i] %= 5
@@ -101,12 +132,12 @@ if __name__ == '__main__':
 
     # e
     print()
-    print("zakodowana macierz A")
+    print("coded matrix A")
     coded_A = z_n_field(code_matrix(A, matrix_G), 5)
     for i in coded_A:
         print(i)
 
-    print("\n wysłana macierz A")
+    print("\n sent matrix A")
     sent_A = send(coded_A)
     for i in sent_A:
         print(i)
